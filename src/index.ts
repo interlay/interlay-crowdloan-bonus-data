66f24f9f3795ea65d41e514c10d5c8b6964ca829
ns212 <nikolai@interlay.io>
2022-08-03 16:09:26 +0100

    Disable testnetFetcher

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import { app } from "./app";
 import { ENDPOINT_URL, PORT } from "./constants";
-import { readQuizCsv, setupListener, testnetFetcher } from "./fetch";
+import { readQuizCsv } from "./fetch";
 import logger from "./util/logger";
 
 function main() {
     logger.info("Service started");
     readQuizCsv();
-
-    // Query every 60 seconds
-    setupListener(testnetFetcher, 300 * 1000);
 }
 
 app.listen(PORT, () =>
